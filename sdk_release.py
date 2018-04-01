070d5a21f061584bb81a8afd06aa4010319f51cc
Blake O'Hare <mspaintmaestro@gmail.com>
2018-04-01 10:45:09 +0900

    TwoCans SDK -> Fix infinite event queue problem. Also create SDK deploy script.

---
+++ sdk_release.py
@@ -0,0 +1,44 @@
+import os
+import zipfile
+
+SDK_NAME = 'TwoCansAlpha1'
+SDK_DIR = 'api/' + SDK_NAME
+
+def is_directory(path): return os.path.isdir(path.replace('/', os.sep))
+def directory_list_files(path): return os.listdir(path.replace('/', os.sep))
+def ensure_directory_exists(path):
+	if not is_directory(path):
+		sep_index = path.rfind('/')
+		if sep_index == -1:
+			subpath = path[:sep_index]
+		else:
+			subpath = '.'
+		ensure_directory_exists(subpath)
+		os.mkdir(path.replace('/', os.sep))
+		
+def gather_files(absolute_path, relative_path, output):
+	for file in directory_list_files(absolute_path):
+		full_abs_path = absolute_path + '/' + file
+		full_rel_path = relative_path + '/' + file
+		if is_directory(full_abs_path):
+			gather_files(full_abs_path, full_rel_path, output)
+		else:
+			# TODO: skip banned files
+			output.append((full_abs_path, full_rel_path))
+			
+
+def main():
+	if not is_directory(SDK_DIR):
+		print("Must run this from the TwoCans repository root.")
+		return
+	
+	files = []
+	gather_files(SDK_DIR, SDK_NAME, files)
+	ensure_directory_exists('./gen-sdk')
+	archive = zipfile.ZipFile(os.path.join('gen-sdk', SDK_NAME + '.zip'), 'w', zipfile.ZIP_DEFLATED)
+	for abs_path, rel_path in files:
+		archive.write(abs_path.replace('/', os.sep), rel_path)
+	archive.close()
+			
+	
+main()
